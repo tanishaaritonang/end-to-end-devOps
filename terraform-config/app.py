86fee1945d1b22d239a2c5773d8dc7d9410a8499
tanishaaritonang <tanishaartng@gmail.com>
2025-12-11 20:22:52 +0700

    adding

--- terraform-config/app.py
+++ terraform-config/app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_CurrentTime():
-    return f"Current Server Time: {time.strftime('%Y-%m-%d %H:%M:%S')}"
+    return f"Current Server Time of the day: {time.strftime('%Y-%m-%d %H:%M:%S')}"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
 
-    # Run the Flask app on all available IPs on port 808000
+    # Run the Flask app on all available IPs on port 80800
