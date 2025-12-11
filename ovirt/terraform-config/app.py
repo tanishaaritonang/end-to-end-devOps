7217b0e6319ecaf12d41009a9adfbf48c57fcef3
tanisha natalia <148616765+tanishaaritonang@users.noreply.github.com>
2025-12-11 22:08:20 +0700

    hhh

---
+++ ovirt/terraform-config/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_CurrentTime():
+    return f"Current Server Time of the day is: {time.strftime('%Y-%m-%d %H:%M:%S')}"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
+
+    # Run the Flask app on all available IPs on port 80800
