0c17d004cd641b4522aab7d19c401e1102ffa0bf
13323046_TANISHA NATALIA ARITONANG <148616765+tanishaaritonang@users.noreply.github.com>
2025-12-11 11:08:50 +0700

    lkkkk

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -1,61 +1,21 @@
-############################################
-# Terraform Backend + Providers
-############################################
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.25"
+terraform{
+    required_providers {
+      minikube = {
+        source = "scott-the-programmer/minikube"
+        version = "0.4.2"
+      }
     }
-  }
 }
 
-############################################
-# Run Minikube (via local-exec)
-############################################
-resource "null_resource" "minikube" {
-  triggers = {
-    always_run = timestamp()
-  }
-
-  # Start Minikube
-  provisioner "local-exec" {
-    command = <<-EOT
-      echo "[Starting Minikube]"
-      minikube start --driver=docker --kubernetes-version=v1.34.0 --cpus=2 --memory=4096 --addons=default-storageclass,storage-provisioner
-    EOT
-
-    # Increase timeout for Minikube startup (default is 5 minutes)
-    interpreter = ["/bin/sh", "-c"]
-    timeout     = "10m"
-  }
-
-  # Delete Minikube on terraform destroy
-  provisioner "local-exec" {
-    when    = destroy
-    command = "minikube delete"
-  }
+provider "minikube" {
+    kubernetes_version = "v1.30.0"
 }
 
-############################################
-# Kubernetes Provider (connected to Minikube)
-############################################
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-
-  # Ensure Minikube is started *before* using Kubernetes provider
-  alias = "mk"
-}
-
-############################################
-# Example Kubernetes Resource
-############################################
-resource "kubernetes_namespace" "example" {
-  provider = kubernetes.mk
-
-  metadata {
-    name = "dev"
-  }
-
-  depends_on = [null_resource.minikube]
+resource "minikube_cluster" "minikube_docker" {
+    driver = "docker"
+    cluster_name = "complete-devops-project"
+    addons = [
+        "default-storageclass",
+        "storage-provisioner"
+    ]
 }
